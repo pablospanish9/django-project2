889cdcddafceb9404289e8dad7e80e13830bd6a0
Pablo Sepulveda <pablolsep@msn.com>
2024-03-09 23:02:07 +0000

    Adds myapp, a simple Django app

---
+++ myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+  return HttpResponse("<h1>Hello Django!</h1>")
+
+def about(request):
+  return HttpResponse("<h1>About Page!</h1>")
+
+def services(request):
+  return HttpResponse("<h1>Services Page!</h1>")
+
+def contact(request):
+  return HttpResponse("<h1>Contact Page!</h1>")
