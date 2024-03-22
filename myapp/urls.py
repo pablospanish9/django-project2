6f316a94b2b7df4336d0d7ea5f40c99bfc5eedbc
Pablo Sep <97714333+pablospanish9@users.noreply.github.com>
2024-03-22 02:11:33 +0000

    inventionds CRUD

--- myapp/urls.py
+++ myapp/urls.py
@@ -14,8 +14,12 @@ urlpatterns = [
   path('class/', views.ClassView.as_view(), name='class_view'),
   path('theme/', views.ThemeView.as_view(), name='theme'),
   path('load/', views.load_default_data_view, name='load_default_data'),
-  path('inventions', views.InventionListView.as_view(), name='invention-list'),
+  path('inventions/', views.InventionListView.as_view(), name='invention-list'),
   path('invention/<int:pk>/', views.InventionDetailView.as_view(), name='invention-view'),
+  path('invention/create/', views.InventionCreateView.as_view(), name='create_invention'),
+  path('invention/<int:pk>/update/', views.InventionUpdateView.as_view(), name='update_invention'),
+  path('invention/<int:pk>/delete/', views.InventionDeleteView.as_view(), name='delete_invention'),
+
 
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -11,6 +11,14 @@ from django.urls import reverse_lazy
 from .models import Invention
 from django.views.generic import DetailView
 
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Invention, Category
+from .forms import InventionForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 # Create your views here.
 
 
@@ -116,5 +124,21 @@ class InventionDetailView(DetailView):
     template_name = 'invention_view.html'
     context_object_name = 'invention'
 
+
+class InventionCreateView(LoginRequiredMixin, CreateView):
+  model = Invention
+  form_class = InventionForm
+  template_name = 'create_invention.html'
+  success_url = reverse_lazy('invention-list')
+
+class InventionUpdateView(LoginRequiredMixin, UpdateView):
+  model = Invention
+  form_class = InventionForm
+  template_name = 'update_invention.html'
+  success_url = reverse_lazy('invention-list')
+
+class InventionDeleteView(LoginRequiredMixin, DeleteView):
+  model = Invention
+  success_url = reverse_lazy('invention-list')
 
 
